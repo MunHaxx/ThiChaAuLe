b86c011403f8647c48f77aabe370cff7ea7691d6
DE COLIGNY Thibault <thibault.2coligny@gmail.com>
2023-04-28 18:40:15 +0200

    First commit : Just the App with "HELLO WORLD"

---
+++ flask-app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__, static_folder='./build/static', template_folder='./build')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/data')
+def get_data():
+    data = {'key': 'value'}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
