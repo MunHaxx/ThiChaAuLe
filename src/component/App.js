836a3b3dab84c7db13ab5517be5bffe3076b39de
DE COLIGNY Thibault <thibault.2coligny@gmail.com>
2023-04-28 23:48:18 +0200

    J'apprends le code à Noah

--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <div>My name is Noah</div>
+      {/* <div>My name is Noah</div> */}
+      <div className="bonjour">Bonjour à tous</div>
+      <div className="containerBulbizarre">
+        <div className='carréDansLaxe'>
+          <img alt="" className="bulbizarre" src='https://www.pokepedia.fr/images/thumb/e/ef/Bulbizarre-RFVF.png/250px-Bulbizarre-RFVF.png' />
+        </div>
+
+        <div className="descriptionBulbizarre">
+          <h1>Bulbizarre</h1>
+          <p>Bulbizarre est un pokémon de la première gen de type plante</p>
+        </div>
+      </div>
+      <a href="https://eu.shop.battle.net/fr-fr">Battle net</a>
+      
     </div>
   );
 }
