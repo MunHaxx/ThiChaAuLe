472273334172fc10f575a44fce488166811154b8
DE COLIGNY Thibault <thibault.2coligny@gmail.com>
2023-05-03 11:40:48 +0200

    test

--- startPauvre.cmd
+++
@@ -1,1 +0,0 @@
-npm run build & xcopy /E /I /Y "C:\Users\chloe\Dropbox\Travail\L3\S6\Web\Projet\ThiChaAuLe\build" "C:\Users\chloe\Dropbox\Travail\L3\S6\Web\Projet\ThiChaAuLe\flask-app\build"
--- startRiche.sh
+++
@@ -1,9 +0,0 @@
-npm run build; 
-cp -r build flask-app/; 
-cd flask-app; 
-flask run &
-
-sleep 2
-open "http://127.0.0.1:5000";
-read -p "********************************************"$'\n'"Appuyez sur Entrée pour fermer l'application"$'\n'"********************************************"$'\n'
-kill -9 $(lsof -t -i:5000)
