209870875ab00d5376ea07d7bfe285444214b6c3
Aunorine <chloe.bastong@efrei.net>
2023-05-15 13:46:21 +0200

    fct delete user, login fonctionnelle Réorganisation des fonctions

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,34 +1,21 @@
 from flask import Flask, jsonify, render_template,request,redirect,url_for,session
 import json
-import paypalrestsdk
-import bcrypt
 import stripe
 
 # ça manque de front par ici
 
 
 app = Flask(__name__, static_folder='./build/static', template_folder='./build')
-app.secret_key  = 'my super secret key'.encode('utf8')
+app.secret_key = 'my super secret key'.encode('utf8')
 
 # configuration de l'interface de paiement
 stripe.api_key = "secret_key"
 
 
-# acceuil
+# accueil
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# tableau de bord user avec panier ect
-@app.route('/user/user=<user>')
-def userdashboard(user):
-
-    with open("users.json", "r") as f:
-        # on récupére les users sous forme de dico
-        data = json.load(f)
-
-    if data["users"][user]["panier"]["Vide"] == "False":
-        return data["users"][user]["panier"]
 
 
 # page de paiement
@@ -53,7 +40,6 @@ def payment_succes():
     render_template("index.html")
 
 
-
 # Page d'inscription pour les utilisateurs
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -62,7 +48,7 @@ def register():
     if request.method == 'POST':
         id = request.form['id']
         password = request.form['password']
-        print(password)
+        # TEST print(password)
 
         # on ajoute le nouvelle user au fichier json des users
         user = {
@@ -79,7 +65,7 @@ def register():
             # on récupére les users sous forme de dico
             data = json.load(f)
             # on rajoute le new user au dico
-            data["users"].update(user)
+            data["users_list"]["user"].update(user)
 
             # on écrase le fichier user avec les users + le new user
             with open("users.json", "w") as f:
@@ -108,39 +94,38 @@ def login():
             data = json.load(f)
 
         # pour chacun de mes user
-        for user in data["users"].keys():
+        if id in data["users_list"]["user"]:
             # si c'est le user qui se connecte
-            if user == id:
-                # si le mdp est correct
-                if data["users"][user]['password'] == password:
-                    # je crée la session associé au id (admin ou users)
-                    session['id'] = id
-                    # je redirige vers le site web adapté à la personne connecter (admin ou users)
-                    return redirect(url_for('dashboard'))
-        # si aucun user ne correspond on renvoi à la connexion et on indique que c'est incorrect
-        return render_template('index.html', error="Email ou mot de passe incorrect")
+            user = id
+            # si le mdp est correct
+            if data["users_list"]["user"][user]['password'] == password:
+                # je crée la session associé au id (admin ou users)
+                session['id'] = id
+                session['type'] = "user"
+
+                # je redirige vers le site web adapté à la personne connecter (user)
+                return redirect(url_for('dashboard'))
+
+        elif id in data["users_list"]["admin"]:
+            user = id
+            # si admin
+            if data["users_list"]["admin"][user]['password'] == password:
+                # je crée la session associé au id (admin ou users)
+
+                session['id'] = id
+                session['type'] = "admin"
+                # je redirige vers le site web adapté à la personne connecter (admin)
+                print("Vous êtes connecté en tant que ", session['id'])
+                return redirect(url_for('dashboard'))
+
+        else:
+            # ERREUR
+
+            # si aucun user ne correspond on renvoi à la connexion et on indique que c'est incorrect
+            return render_template('index.html', error="Email ou mot de passe incorrect")
 
     # on attend que le formulaire sois remplis
     return render_template('index.html')
-
-# tableau de bord admin avec gestion des users et commandes
-@app.route('/admin')
-def admin():
-    return render_template('index.html')
-
-
-# Tableau de bord
-@app.route('/dashboard')
-def dashboard():
-    # on verifie que le user est co
-    if 'id' in session:
-        if 'id' == "admin":
-            return redirect(url_for('admin'))
-        else :
-            return redirect(url_for('userdashboard' , user = id ))
-    # sinon on lui dis de se co
-    else:
-        return redirect(url_for('login'))
 
 
 # Déconnexion de l'utilisateur
@@ -149,6 +134,81 @@ def logout():
     # si on se déco on détruit la session et retourne à l'acceuil
     session.clear()
     return redirect(url_for('/'))
+
+
+# tableau de bord admin avec gestion des users et commandes
+@app.route('/admin')
+def admin():
+    return render_template('index.html')
+
+
+# tableau de bord user avec panier ect
+@app.route('/delete_user/<usertodel>')
+def deleteuser(usertodel):
+    print("Vous êtes ", session['id'])
+    if session['type'] == "admin":
+        print("session['type'] == admin")
+        if usertodel == "superviseur":
+            # ERREUR
+            print("On ne peut pas suprimer le superviseur")
+        else:
+            # ouvre JSON
+            with open("users.json", "r") as f:
+                # on récupère les users sous forme de dico
+                data = json.load(f)
+
+            # Recherche le user à suppr
+            if usertodel in data['users_list']["admin"]:
+                # Supprimer l'utilisateur
+                del data['users_list']["admin"][usertodel]
+
+                # on écrase le fichier user avec les users - le user supprimé
+                with open("users.json", "w") as f:
+                    f.write("")
+                    json.dump(data, f)
+
+            elif usertodel in data['users_list']["user"]:
+                # Supprimer l'utilisateur
+                del data['users_list']["user"][usertodel]
+
+                # on écrase le fichier user avec les users - le user supprimé
+                with open("users.json", "w") as f:
+                    f.write("")
+                    json.dump(data, f)
+    else:
+        # ERREUR !!!!
+        print("erreur : vous n'êtes pas admin")
+
+    return render_template('index.html')
+
+
+# Tableau de bord
+@app.route('/dashboard')
+def dashboard():
+    # on verifie que le user est co
+    if 'id' in session:
+        if session['type'] == "admin":
+            return redirect(url_for('admin'))
+        else :
+            return redirect(url_for('userdashboard' , user = id ))
+    # sinon on lui dis de se co
+    else:
+        return redirect(url_for('login'))
+
+
+# tableau de bord user avec panier ect
+@app.route('/userdashboard/<user>')
+def userdashboard(user):
+
+    with open("users.json", "r") as f:
+        # on récupère les users sous forme de dico
+        data = json.load(f)
+
+    #if data["users_list"][user]["panier"]["Vide"] == "False":
+     #   return data["users"][user]["panier"]
+
+    return render_template('index.html')
+
 
 # lancement de l'appli flask
 if __name__ == '__main__':
