7163aa4b3812accdb033b3d004abb1c7846718bc
LeozeCroissant <leo.foulloy@gmail.com>
2023-05-15 15:53:54 +0200

    Création page Description

--- src/Pages/HomePage/Description.js
+++ src/Pages/HomePage/Description.js
@@ -7,7 +7,14 @@ function Description() {
     <div className="Description">
         <img alt="" src={imgDescription} />
         <div className="right-part-container">
-          <h1>Description</h1>
+          <div className='in-a-box'>Qu'y a-t-il à l'intérieur de chaque Box ?</div>
+          <ul>
+            <li>Des produits issus de producteurs français</li>
+            <li>Des explications sur les produits</li>
+            <li>La France a porté de main</li>
+            <li>Délicieux repas garantis !</li>
+          </ul>
+          <button>PARCOURIR LES BOXS !</button>
         </div>
     </div>
   )
