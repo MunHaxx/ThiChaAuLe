d54ce87ca9906de5e31b338dc96d0987e25964bd
LeozeCroissant <leo.foulloy@gmail.com>
2023-05-21 15:01:29 +0200

    Register page (misses change of formulaire button)

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Formulaire from '../component/Formulaire';
+import { Link } from 'react-router-dom'
 
 import "./Login.css";
 
@@ -7,15 +8,22 @@ function Login() {
 
   return (
     <div className="Login">
-        <div className="login-titles">
+        <div className="login-and-register">
           <div className="login-site-title">
             <span className="the">THE</span>  
             <span className="french">FRENCH</span> 
             <span className = "flavor">FLAVOR</span>
           </div>
           <div className="login-connecter-title">SE CONNECTER</div>
+          <div className="Formulaire-Login"><Formulaire /></div>
+          <div className="other-register">
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><button className="oublie-mdp" >Mot de passe oublié ?</button></a> 
+              <div className="pas-de-compte">Vous n'avez pas de compte ?</div>
+              <Link to="/register"><button className="inscrivez-vous" >Inscrivez-vous !</button></Link>
+            </div>
         </div>
-        <Formulaire />
+
+       
 
     </div>
   );
--- src/component/Formulaire.js
+++ src/component/Formulaire.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Formulaire.css"
-import { Link } from 'react-router-dom'
 
 function Formulaire() {
 
@@ -11,11 +10,6 @@ function Formulaire() {
             <input className="id-password-login" type="text" name="id" placeholder="nom d'utilisateur"/>
             <input className="id-password-login" type="password" name="password" placeholder="mot de passe"/>
             <button className="connect-login" type="submit" value="valider">Connexion</button>
-            <div className="other-register">
-              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><button className="oublie-mdp" >Mot de passe oublié ?</button></a> 
-              <div className="pas-de-compte">Vous n'avez pas de compte ?</div>
-              <Link to="/register"><button className="inscrivez-vous" >Inscrivez-vous !</button></Link>
-            </div>
           </form>
         </div>
       </div>
